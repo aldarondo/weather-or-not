8310dc0de4b1d77516cc91568e820ffbf95b8f13
caldarondo <caldarondo@godaddy.com>
2014-10-26 21:06:56 -0700

    Temperature Controller

---
+++ Weather.Tests/Controllers/TemperatureControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Weather;
+using Weather.Controllers;
+
+namespace Weather.Tests.Controllers
+{
+  [TestClass]
+  public class TemperatureControllerTest
+  {
+
+    [TestMethod]
+    public void TemperaturePut()
+    {
+      // Arrange
+      TemperatureController controller = new TemperatureController();
+
+      //Act
+      controller.Put("location", "98.6");
+    }
+
+    [TestMethod]
+    public void TemperatureGet()
+    {
+      // Arrange
+      TemperatureController controller = new TemperatureController();
+      controller.Put("location", "98.6");
+
+      //Act
+      string result = controller.Get("location");
+
+      Assert.AreEqual("98.6", result);
+    }
+  }
+}
---
+++ Weather/Controllers/TemperatureController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Weather.Controllers
+{
+    public class TemperatureController : ApiController
+    {
+      private string locationValue;
+
+      // PUT api/values/5
+      [Route("temp/{location}/{value}")]
+      [HttpPut]
+      public void Put(string location, string value)
+      {
+        locationValue = value;
+      }
+
+      [Route("temp/{location}")]
+      [HttpGet]
+      public string Get(string location)
+      {
+        return locationValue;
+      }
+    }
+}
